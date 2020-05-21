f03ebce182fdefa4d80a7cadd901816b32edf210
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-21 14:15:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,8 +12,12 @@ def loop_message_n_times(message, int)
   end
 end
 
-def output_array
-  
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
 end
 
 def return_string_array
