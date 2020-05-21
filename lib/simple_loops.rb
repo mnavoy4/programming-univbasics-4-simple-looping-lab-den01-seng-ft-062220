f203214196010f4da095ab0ebdb590ce83e132ac
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-21 14:14:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,21 @@
 # Write your methods here
 
-def loop_message_five_times
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, int)
+  int.times do
+    puts message
+  end
+end
+
+def output_array
   
 end
 
-def 
+def return_string_array
+  
+end
